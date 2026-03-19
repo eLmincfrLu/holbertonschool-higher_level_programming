fdcb9be4262ad1aa08cb7f6f722f4d0eff47d3e1
eLmincfrLu <elmincfrlu@gmail.com>
2026-03-20 00:44:32 +0400

    python

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     if len_argv < 2:
         print("{} arguments.".format(0))
     else:
-        print("{} argument:".format(len_argv))
+        print("{} argument:".format(len_argv - 1))
         for i in range(1, len_argv):
             print("{}: {}".format(i, sys.argv[i]))
