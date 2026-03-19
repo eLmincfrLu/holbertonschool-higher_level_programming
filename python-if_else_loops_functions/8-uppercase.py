94d18865a733e966a02de61b693f48927158a283
eLmincfrLu <elmincfrlu@gmail.com>
2026-03-19 16:04:06 +0400

    python

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,8 @@
 def uppercase(str):
     nev = ""
     for letter in str:
-        if letter > "a" and letter < "z":
-            nev = nev + chr(ord(letter)-25)
+        if letter >= "a" and letter <= "z":
+            nev = nev + chr(ord(letter)-32)
         else:
             nev = nev + letter
-    return print("{}".format(nev))   
+    return print("{}".format(nev))
