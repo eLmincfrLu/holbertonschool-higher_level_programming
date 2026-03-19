e6f581c1c80ea9b6c70b82a3c64611ba8eed997a
eLmincfrLu <elmincfrlu@gmail.com>
2026-03-20 00:48:14 +0400

    python

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,8 +4,11 @@ import sys
 len_argv = len(sys.argv)
 if __name__ == "__main__":
     if len_argv < 2:
-        print("{} argument.".format(0))
+        print("{} arguments.".format(0))
     else:
-        print("{} arguments:".format(len_argv - 1))
+        if len_argv < 3:
+            print("{} argument:".format(len_argv - 1))
+        else:
+            print("{} arguments:".format(len_argv - 1))
         for i in range(1, len_argv):
             print("{}: {}".format(i, sys.argv[i]))
