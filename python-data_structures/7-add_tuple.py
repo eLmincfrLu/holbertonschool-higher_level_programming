b032032b95af7a79677dcac768b7ace4fb093639
eLmincfrLu <elmincfrlu@gmail.com>
2026-03-20 21:26:17 +0400

    pyhton

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,16 +4,20 @@ def add_tuple(tuple_a=(), tuple_b=()):
         a1 = tuple_a[0]
     else:
         a1 = 0
+
     if len(tuple_a) > 1:
         a2 = tuple_a[1]
     else:
         a2 = 0
+
     if len(tuple_b) > 0:
         b1 = tuple_b[0]
     else:
         b1 = 0
+
     if len(tuple_b) > 1:
         b2 = tuple_b[1]
     else:
         b2 = 0
+
     return (a1 + b1, a2 + b2)
