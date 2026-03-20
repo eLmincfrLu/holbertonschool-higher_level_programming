3b9a34dc63b9706d3c239fac23440e0360bd3207
eLmincfrLu <elmincfrlu@gmail.com>
2026-03-20 22:57:33 +0400

    pyhton

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    if n > len(str) - 1:
-        return str
     nev_str = list(str)
-    del nev_str[n]
-    return "".join(nev_str)
+    if (n > len(nev_str) - 1):
+        return str
+    if (-len(nev_str)) < abs(n) and n < 0:
+        return str
+    nev_str[n] = ""
+    return ("".join(nev_str))
+
+print(remove_char_at("Best School", 3))
+print(remove_char_at("Chicago", 2))
+print(remove_char_at("C is fun!", 0))
+print(remove_char_at("School", 10))
+print(remove_char_at("Python", -2))
