717c555352b70a73d3cf1de45b276666d31e7d82
eLmincfrLu <elmincfrlu@gmail.com>
2026-03-20 23:01:03 +0400

    pyhton

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -3,5 +3,7 @@ def remove_char_at(str, n):
     nev_str = list(str)
     if (n > len(nev_str) - 1):
         return str
+    if (-len(nev_str)) < abs(n) and n < 0:
+        return str
     nev_str[n] = ""
     return ("".join(nev_str))
