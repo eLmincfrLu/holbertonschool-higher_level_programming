67be4f0d576800a6b5dd190d97fa4b83b72ef05f
eLmincfrLu <elmincfrlu@gmail.com>
2026-03-20 19:37:01 +0400

    pyhton

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
-            print("{:d}".format(num), end=" ")
+            if row.index(num) == len(row) - 1:
+                print("{:d}".format(num))
+            else:
+                print("{:d} ".format(num), end=" ")
         print()
-
