b88edc0aa2ca30711b50049167f5ffe1450b74d5
eLmincfrLu <elmincfrlu@gmail.com>
2026-03-20 19:29:41 +0400

    pyhton

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
-            print("{}".format(num), end=" ")
+            print("{:d}".format(num), end=" ")
         print()
 
