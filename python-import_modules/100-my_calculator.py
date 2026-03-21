bc1a6d5602fb3d1202060a29e335ef87340bcca3
eLmincfrLu <elmincfrlu@gmail.com>
2026-03-21 13:25:47 +0400

    pyhton

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-from sys import sys
+from sys import argv, exit
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    first = sys.argv[1]
-    second = sys.argv[3]
-    if sys.argv[2] == "+":
+        exit(1)
+    first = argv[1]
+    second = argv[3]
+    if argv[2] == "+":
         print("{} + {} = {}".format(first, second, add(int(first), int(second))))
-    elif sys.argv[2] == "-":
+    elif argv[2] == "-":
         print("{} - {} = {}".format(first, second, sub(int(first), int(second))))
-    elif sys.argv[2] == "*":
+    elif argv[2] == "*":
         print("{} * {} = {}".format(first, second, mul(int(first), int(second))))
-    elif sys.argv[2] == "/":
+    elif argv[2] == "/":
         print("{} / {} = {}".format(first, second, div(int(first), int(second))))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
