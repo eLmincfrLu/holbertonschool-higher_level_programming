6c8025502d4d5ec7f822041a8f4bb3b63ed22dd5
eLmincfrLu <elmincfrlu@gmail.com>
2026-03-21 13:16:56 +0400

    pyhton

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,11 +3,11 @@ import sys
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    first = sys.argv[1]
-    second = sys.argv[3]
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    first = sys.argv[1]
+    second = sys.argv[3]
     if sys.argv[2] == "+":
         print("{} + {} = {}".format(first, second, add(int(first), int(second))))
     elif sys.argv[2] == "":
