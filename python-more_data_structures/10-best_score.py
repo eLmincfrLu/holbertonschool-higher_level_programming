b8803af91a69c73ce3f84bb8365807b7cec67173
eLmincfrLu <elmincfrlu@gmail.com>
2026-03-24 15:01:41 +0400

    pyhton

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    maxi_v =  a_dictionary[list(a_dictionary)[0]]
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+    maxi_v = a_dictionary[list(a_dictionary)[0]]
     maxi_k = list(a_dictionary)[0]
     for key, value in a_dictionary.items():
         if value > maxi_v:
