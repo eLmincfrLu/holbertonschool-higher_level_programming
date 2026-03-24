323889dbdf2ce43b247f46e3eaed0daa0c9075ec
eLmincfrLu <elmincfrlu@gmail.com>
2026-03-24 16:05:16 +0400

    pyhton

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) != str:
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
-    roma_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roma_dict = {"I": 1, "V": 5, "X": 10,
+                  "L": 50, "C": 100, "D": 500, "M": 1000}
 
     sum = 0
 
