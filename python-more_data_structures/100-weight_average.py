d411d20524e5c48cd1971d8d77cfaf1960991fe1
eLmincfrLu <elmincfrlu@gmail.com>
2026-03-24 16:21:46 +0400

    pyhton

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    sum = 0
+    count = 0
+    for row in my_list:
+        sum += row[0] + row[1]
+        count += row[1]
+    return sum / count
