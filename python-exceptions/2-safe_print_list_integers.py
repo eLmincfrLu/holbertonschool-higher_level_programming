35b91858bb1be1c9fec4f861a381f54dba6347ed
eLmincfrLu <elmincfrlu@gmail.com>
2026-03-25 16:44:43 +0400

    pyhton

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    """Prints the first x elements of a list if they are integers.
-    Returns the number of integers printed.
-    """
-    count = 0  # çapa çıxan integerlərin sayı
+    count = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end = "")
             count += 1
-        except (ValueError, TypeError):
-            # integer olmayan elementləri sadəcə keçirik
-            continue
         except IndexError:
-            # x list-in uzunluğundan böyükdürsə, try-except ilə dayandırırıq
             break
+        except (TypeError, ValueError):
+            continue
     print()
     return count
