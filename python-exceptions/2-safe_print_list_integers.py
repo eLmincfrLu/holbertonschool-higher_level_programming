d166426e7fdca813c0707b8bcd23fb10a3d0d1d6
eLmincfrLu <elmincfrlu@gmail.com>
2026-03-25 16:34:03 +0400

    pyhton

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,10 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
-
+        try:
+            valuee = my_list[i]
+        except IndexError:
+            break
         try:
             print("{:d}".format(my_list[i]), end = "")
             count += 1
