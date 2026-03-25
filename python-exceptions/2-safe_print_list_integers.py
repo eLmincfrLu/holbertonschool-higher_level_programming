99629ebeb2893d870b333620052c1dca73ceb391
eLmincfrLu <elmincfrlu@gmail.com>
2026-03-25 16:53:28 +0400

    pyhton

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,12 +6,10 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except (ValueError, TypeError):
-            # integer olmayan elementləri atla
+        except TypeError:
             pass
         except IndexError:
-            # x list uzunluğunu keçirsə dayandır
             break
         i += 1
-    print()  # sonda newline
+    print()
     return count
