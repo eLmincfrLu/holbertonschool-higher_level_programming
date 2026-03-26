cdbc0a152c27df9134efc44004b4a523ed651735
eLmincfrLu <elmincfrlu@gmail.com>
2026-03-26 17:15:39 +0400

    pyhton

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Module that adds two integers."""
+
+
 def add_integer(a, b=98):
-    """Adds two integers and returns the result"""
+    """Adds two integers and returns the result.
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return a + b
+    return int(a) + int(b)
