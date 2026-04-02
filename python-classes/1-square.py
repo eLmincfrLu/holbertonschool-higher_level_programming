b6417426af0a589bb7962b66c6cba4c686c6680c
eLmincfrLu <elmincfrlu@gmail.com>
2026-04-02 22:26:50 +0400

    pyhton

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """represents  attributes"""
+        self.__size = size
