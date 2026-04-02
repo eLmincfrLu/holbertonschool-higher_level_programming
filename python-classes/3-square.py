b36e913c77686dd059137eaa3f7429a33254d387
eLmincfrLu <elmincfrlu@gmail.com>
2026-04-02 23:14:14 +0400

    pyhton

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -12,6 +12,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     def area(self):
         """represent method"""
         return self.__size ** 2
