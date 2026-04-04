8f8188cb65ce375fe6b7197e9c261290df2ea86e
eLmincfrLu <elmincfrlu@gmail.com>
2026-04-04 17:18:54 +0400

    pyhton

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -46,4 +46,4 @@ class Rectangle:
         """Calculates the perimeter"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__height + self.__height)
+        return 2 * (self.__height + self.__width)
