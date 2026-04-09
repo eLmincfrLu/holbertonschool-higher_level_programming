364d8367b7ededf1ce890e48b48ea46f933c05d2
eLmincfrLu <elmincfrlu@gmail.com>
2026-04-09 23:21:06 +0400

    pyhton

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,8 +5,8 @@ This module defines a class MyList that inherits from list.
 
 
 class MyList(list):
-    """A subclass of list that provides a sorted printing method."""
+    """A subclass of list that provides a sorted """
 
     def print_sorted(self):
-        """Prints the list in ascending order without modifying the original."""
+        """Prints the modifying the original."""
         print(sorted(self))
