c40556401500d6b39301fc888613b26d6bdb3701
eLmincfrLu <elmincfrlu@gmail.com>
2026-04-10 21:55:59 +0400

    pyhton

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""ftghs"""
+
+
+class BaseGeometry:
+    """hsdg"""
+    def area(self):
+        """rjkhegfds"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """dhsilud"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(self.name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(self.name))
