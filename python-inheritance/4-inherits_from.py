95dd138fba73c4a5b3f8d07b2494ce7a0acea4fb
eLmincfrLu <elmincfrlu@gmail.com>
2026-04-10 21:00:03 +0400

    pyhton

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,6 +4,6 @@
 
 def inherits_from(obj, a_class):
     """RUGEDFB"""
-    if issubclass(obj, a_class):
+    if isinstance(obj, a_class) and type(obj) != a_class:
         return True
     return False
