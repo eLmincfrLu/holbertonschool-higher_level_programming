ad5d0ff3688a8919f0afc2c8817a04fcc490b634
eLmincfrLu <elmincfrlu@gmail.com>
2026-04-11 00:15:00 +0400

    pyhton

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -9,7 +9,9 @@ class Square(Rectangle):
         """iufesdg"""
         self.integer_validator("size", size)
         self.__size = size
+
     def area(self):
         return self.__size ** 2
+
     def __str__(self):
         return "[Square] {}/{}".format(self.__size, self.__size)
