8d7bcf9270b3e90c01da08b345daf62a5d2c09b1
eLmincfrLu <elmincfrlu@gmail.com>
2026-04-12 02:57:35 +0400

    pyhton

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""jdf"""
+
+
+class SwimMixin:
+    """ingf"""
+    def swim(self):
+        print("The creature swims!")
+
+class FlyMixin:
+    """kjbewdsj"""
+    def fly(self):
+        print("The creature flies!")
+
+class Dragon(SwimMixin, FlyMixin):
+    """jerds"""
+    def roar(self):
+        print("The dragon roars!")
