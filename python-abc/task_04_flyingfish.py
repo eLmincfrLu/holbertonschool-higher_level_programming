b2b41d28d8efeb0d16afed4a00820d42a41717b2
eLmincfrLu <elmincfrlu@gmail.com>
2026-04-12 02:52:31 +0400

    pyhton

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""jrns"""
+
+
+class Fish:
+    """hfd"""
+    def swim(self):
+        print("The fish is swimming")
+    def habitat(self):
+        print("The fish lives in water")
+
+class Bird:
+    """djfbn"""
+    def fly(self):
+        print("The bird is flying")
+    def habitat(self):
+        print("The bird lives in the sky")
+
+class FlyingFish:
+    """kmfmd"""
+    def fly(self):
+        print("The flying fish is soaring!")
+    def swim(self):
+        print("The flying fish is swimming!")
+    def habitat(self):
+        print("The flying fish lives both in water and the sky!")
