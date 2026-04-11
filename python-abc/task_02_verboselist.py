0fbf5ad1281e78557c18be6c904837de19060fc7
eLmincfrLu <elmincfrlu@gmail.com>
2026-04-12 02:22:05 +0400

    pyhton

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""jdfs"""
+
+from abc import ABC, abstractmethod
+
+
+class VerboseList(list):
+    """df"""
+    def append(self, item):
+        super().append(item)
+        print("Added [{}] to the list.".format(item))
+    def extend(self, list2):
+        x = len(list2)
+        super().extend(list2)
+        print("Extended the list with [{}] items.".format(x))
+    def remove(self, item):
+        super().remove(item)
+        print("Removed [{}] from the list.".format(item))
+    def pop(self, index = -1):
+        a = super().pop(index)
+        print("Popped [{}] from the list.".format(a))
