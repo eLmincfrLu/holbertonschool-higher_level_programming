e01da4e029943cf364125ebd9b0a2ddb80119550
eLmincfrLu <elmincfrlu@gmail.com>
2026-04-19 10:26:19 +0400

    pyhton

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""ghjsk"""
+'''This module writes to text file.'''
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """sfsff"""
-    with open(filename, "a", encoding="utf-8") as f:
+    '''Writes to text file.'''
+    with open(filename, mode="w", encoding="utf-8") as f:
         json.dump(my_obj, f)
--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-"""
-Komanda sətrindən gələn bütün arqumentləri Python siyahısına (list) əlavə edir
-və onları JSON faylında saxlayır.
-"""
+'''This module adds all arguments
+to a Python list, and then them to a file.'''
+import json
 import sys
-import os.path
-
-# Əvvəlki tapşırıqlarda yazdığımız funksiyaları import edirik
+import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-# 1. Əgər fayl mövcuddursa, mövcud siyahını yükləyirik
-if os.path.exists(filename):
-    items = load_from_json_file(filename)
-else:
-    # 2. Fayl yoxdursa, boş bir siyahı ilə başlayırıq
-    items = []
+try:
+    my_list = load_from_json_file(filename)
+except FileNotFoundError:
+    my_list = []
 
-# 3. Komanda sətrindən gələn arqumentləri (skriptin adından başqa) siyahıya əlavə edirik
-# sys.argv[1:] skriptin adını keçib, yalnız ötürülən sözləri götürür
-items.extend(sys.argv[1:])
+my_list.extend(sys.argv[1:])
 
-# 4. Yenilənmiş siyahını yenidən JSON faylına yazırıq
-save_to_json_file(items, filename)
+save_to_json_file(my_list, filename)
