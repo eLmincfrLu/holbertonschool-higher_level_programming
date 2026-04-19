da7a7f77058370e1298dc35d5c5f45ccead10647
eLmincfrLu <elmincfrlu@gmail.com>
2026-04-19 11:44:42 +0400

    pyhton

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -9,14 +9,10 @@ def pascal_triangle(n):
     triangle = [[1]]
     for i in range(1, n):
         evvelki_row = triangle[i - 1]
-        
         indiki_row = [1]
-
         for j in range(1, i):
             indiki_row.append(evvelki_row[j - 1] + evvelki_row[j])
-
         indiki_row.append(1)
-        
         triangle.append(indiki_row)
 
     return triangle
