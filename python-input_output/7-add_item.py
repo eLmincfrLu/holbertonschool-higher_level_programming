0b70980ff4d1163ecd308cd6ffc20fbac4213fb1
eLmincfrLu <elmincfrlu@gmail.com>
2026-04-19 09:59:18 +0400

    pyhton

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,6 +5,13 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
-with open("add_item.json", "w", encoding="utf-8") as f:
-    for i in sys.argv:
-        save_to_json_file(i, f)
+file = "add_item.json"
+try:
+    items = load_from_json_file(file)
+except Exception:
+      items = []
+
+for i in sys.argv[1:]:
+        items.append(i)
+
+save_to_json_file(items, file)
