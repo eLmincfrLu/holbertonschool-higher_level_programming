0e7c0e17c0812557c6abd14ddc340019b0e2458e
eLmincfrLu <elmincfrlu@gmail.com>
2026-04-19 10:17:50 +0400

    pyhton

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""
-Adds all arguments to a Python list, and then save them to a file
-"""
+"""cfghnjm"""
+
+
 import sys
-
-
+import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-try:
-    items = load_from_json_file(filename)
-except Exception:
-    items = []
+if os.path.exists(filename):
+    my_list = load_from_json_file(filename)
+else:
+    my_list = []
 
-items.extend(sys.argv[1:])
-save_to_json_file(items, filename)
+my_list.extend(sys.argv[1:])
+save_to_json_file(my_list, filename)
