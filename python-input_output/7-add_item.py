dcb5d0a0774f336bd2cfdbef423dd7f406395c67
eLmincfrLu <elmincfrlu@gmail.com>
2026-04-19 10:19:41 +0400

    pyhton

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,9 +10,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 
 if os.path.exists(filename):
-    my_list = load_from_json_file(filename)
+    items = load_from_json_file(filename)
 else:
-    my_list = []
+    items = []
 
-my_list.extend(sys.argv[1:])
-save_to_json_file(my_list, filename)
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
