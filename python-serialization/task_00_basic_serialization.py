46e5abab54b6f995b2ece7aa47809acf6f302bd6
eLmincfrLu <elmincfrlu@gmail.com>
2026-04-19 13:18:54 +0400

    pyhton

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -3,9 +3,11 @@
 import json
 
 def serialize_and_save_to_file(data, filename):
+    """dfghjkl"""
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, filename)
 
 def load_and_deserialize(filename):
+    """fghjkl"""
     with open(filename, "r", encoding="utf-8") as f:
         json.load(filename)
