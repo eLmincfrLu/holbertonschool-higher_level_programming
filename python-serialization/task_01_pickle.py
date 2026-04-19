55628ef65713433c0d180785d0432ef6d2fda0e0
eLmincfrLu <elmincfrlu@gmail.com>
2026-04-19 13:56:50 +0400

    pyhton

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""dfghjkl"""
+import pickle
+
+
+class CustomObject:
+    """fghjkl"""
+    dicti = {}
+    def __init__(self, name, age, is_student):
+        """ghjk"""
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """ghj"""
+        print("Name: {}".format(self.name))
+        print("Age: {}".format(self.age))
+        print("Is Student: {}".format(self.is_student))
+
+    def serialize(self, filename):
+        """fghjk"""
+        with open(filename, "wb") as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, "rb") as f:
+                data = pickle.load(f)
+                return None
+        except Exception:
+            return None
