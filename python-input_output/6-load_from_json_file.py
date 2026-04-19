19c71d10be17cd7f3a302f4c393fdd007b42c8fb
eLmincfrLu <elmincfrlu@gmail.com>
2026-04-19 10:05:41 +0400

    pyhton

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""gdzjdzs"""
+"""udsg"""
 import json
 
 
 def load_from_json_file(filename):
-    """sdd"""
+    """Fdgfsd"""
     with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
