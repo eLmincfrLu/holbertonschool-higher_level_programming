b7257009be2d882d0ff496f007e707a5587159ff
eLmincfrLu <elmincfrlu@gmail.com>
2026-04-19 10:02:31 +0400

    pyhton

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,5 +6,4 @@ import json
 def load_from_json_file(filename):
     """sdd"""
     with open(filename, "r", encoding="utf-8") as f:
-        data = json.load(f)
-        return data
+        return json.load(f)
